378a32059dd59070d2fb3e60dca4578d0a60ea3f
BuckyI <454897555@qq.com>
2022-04-26 17:59:54 +0800

    添加说明等

--- Assets/Script/USV/Navigation.cs
+++ Assets/Script/USV/Navigation.cs
@@ -30,8 +30,8 @@ public class Navigation : MonoBehaviour
             // ATTENTION: 要修改 Kinematic USV 的变量 而非 transform
             // 路径点坐标系和船的坐标系不一样
             Vector2 pos = task.path[task.position];
-            task.ship.GetComponent<KinematicUSV>().x = pos.y;
-            task.ship.GetComponent<KinematicUSV>().y = pos.x;
+            ship.GetComponent<KinematicUSV>().x = pos.y;
+            ship.GetComponent<KinematicUSV>().y = pos.x;
 
             tasks.Add(task);
         }
@@ -62,9 +62,90 @@ public class Navigation : MonoBehaviour
             {
                 float x = (float)Convert.ToDouble(m.Groups[1].Value);
                 float y = (float)Convert.ToDouble(m.Groups[2].Value);
-                single_path.Add(new Vector2(x, y)); // store
+                // waypoints located at (x, z) in Unity
+                single_path.Add(new Vector2(x, y)); // 这里坐标进行转换
             }
         }
         Debug.Log(single_path.Count + " waypoints loaded.");
         return single_path;
     }
+
+}
+
+
+/*
+在 Unity 里面 psi 的位置好像有点不一样
+location x, y
+当前 psi
+path nx2 数组
+
+
+function [u, yaw, reached] = calc_ref(location,psi, path)
+% u: 期望速度
+% yaw: 期望船角度
+% reached: 是否到达终点
+u=0.1;
+yaw=0;
+reached=false;
+
+%% 确定目标点
+% the closest point which has't been accessed 
+persistent k;
+if isempty(k)
+    k=1;
+end
+
+while k<length(path)
+    target=path(k,:);
+    d=target-location'; % 期望更新矢量
+    if sum(d.^2)<25 && k<length(path) % 5m 之内视为已观测, 移到下一个点
+       k=k+1;
+    else
+        break;
+    end
+end
+
+d=path(k,:)-location'; % 期望更新矢量
+if sum(d.^2)<4 % 船距离终点2米, 视为到达终点, 停下来
+    u=0;
+    yaw=0;
+    reached=true;
+    return
+end
+
+%% 计算期望值
+% calculate yaw
+yaw=calc_yaw(d);
+while abs(yaw-psi)>pi
+    if yaw-psi>pi
+        yaw=yaw-2*pi;
+    elseif yaw-psi<-pi
+        yaw=yaw+2*pi;
+    end
+end
+
+% 如果发现目标点在船的后面(有跟踪失败的风险),则策略切换到选择最近点作为目标点
+if abs(yaw-psi)>0.5*pi
+    subpath=path(k:end,:);
+    dir=subpath-location';
+    distance=sum(dir.^2,2); % x^2+y^2
+    min_index = find(distance==min(distance));
+    k=k + min_index(1)-1; % 找到第一个距离最近的索引, 并转化成相对path的索引
+    % 切换 k, 下次更新
+end
+
+% calculate u
+u=log10(sqrt(sum(d.^2,"all"))+1)*(1+cos(yaw-psi))*0.5; % 考虑到距离, 转角确定速度
+
+function yaw=calc_yaw(d)
+    yaw=0;
+    if d(1)>0
+        yaw=atan(d(2)/d(1));
+    elseif d(1)<=0 && d(2) >0
+        yaw=pi+atan(d(2)/d(1));
+    elseif d(1)<=0 && d(2)<=0
+        yaw=-pi+atan(d(2)/d(1));
+    end
+
+
+*/
